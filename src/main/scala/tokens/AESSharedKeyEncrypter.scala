0b5d57fc88798cdc0f9a33eac365d50d1e413ffa
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2013-03-19 14:44:56 +0200

    Moved encryption logic to its own trait

---
+++ src/main/scala/tokens/AESSharedKeyEncrypter.scala
@@ -0,0 +1,34 @@
+package tokens
+
+import javax.crypto.{Cipher, SecretKeyFactory}
+import javax.crypto.spec.{IvParameterSpec, SecretKeySpec, PBEKeySpec}
+
+trait AESSharedKeyEncrypter {
+  val keyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1")
+
+  def passPhrase: String
+
+  def encrypt(data: Array[Byte], salt: Array[Byte]): Array[Byte] = {
+    val spec = new PBEKeySpec(passPhrase.toCharArray, salt, 65536, 128)
+    val tmp = keyFactory.generateSecret(spec)
+    val secret = new SecretKeySpec(tmp.getEncoded, "AES")
+    val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+    cipher.init(Cipher.ENCRYPT_MODE, secret)
+    val params = cipher.getParameters
+    val iv = params.getParameterSpec(classOf[IvParameterSpec]).getIV
+    assert(iv.length == 16)
+    iv ++ cipher.doFinal(data, 0, data.length)
+  }
+
+  def decrypt(encrypted: Array[Byte], salt: Array[Byte]): Array[Byte] = {
+    require(encrypted.length > 16)
+    val iv = encrypted.slice(0, 16)
+    val data = encrypted.slice(16, encrypted.length)
+    val spec = new PBEKeySpec(passPhrase.toCharArray, salt, 65536, 128)
+    val tmp = keyFactory.generateSecret(spec)
+    val secret = new SecretKeySpec(tmp.getEncoded, "AES")
+    val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+    cipher.init(Cipher.DECRYPT_MODE, secret, new IvParameterSpec(iv))
+    cipher.doFinal(data)
+  }
+}
--- src/main/scala/tokens/TokenCreator.scala
+++ src/main/scala/tokens/TokenCreator.scala
@@ -1,18 +1,12 @@
 package tokens
 
-import javax.crypto.spec.SecretKeySpec
-import javax.crypto.spec.IvParameterSpec
-import javax.crypto.Cipher
-import javax.crypto.spec.PBEKeySpec
-import javax.crypto.SecretKeyFactory
 import base58.Base58
 import java.util.Arrays
 import serialization.InvalidDataException
 
-trait TokenCreator extends FieldEncoderTokenEncoder {
+trait TokenCreator extends FieldEncoderTokenEncoder with AESSharedKeyEncrypter {
   val header = "AUTH-TOKEN"
   val version = "1.0"
-  val keyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1")
   
   val encodingCharset: String
   def passPhrase: String
@@ -24,48 +18,23 @@ trait TokenCreator extends FieldEncoderTokenEncoder {
   def createAuthToken(auth: Authentication): String = {
     val tokenData = encodeToken(auth)
     val payload = headerBytes ++ versionBytes ++ tokenData
-    val (iv, encrypted) = encrypt(payload, salt)
-    val tokenWithIv = iv ++ encrypted
-    Base58.encode(tokenWithIv)
+    val encrypted = encrypt(payload, salt)
+    Base58.encode(encrypted)
   }
   
   def decodeAuthToken(tokenString: String): Authentication = {
-    val base58 = Base58.decode(tokenString)
-    val iv = base58.slice(0, 16)
-    val encryptedTokenData = base58.slice(16, base58.length)
-    val decrypted = decrypt(iv, encryptedTokenData, salt)
-    val header = decrypted.slice(0, headerBytes.length)
+    val encryptedTokenData = Base58.decode(tokenString)
+    val decryptedTokenData = decrypt(encryptedTokenData, salt)
+    val header = decryptedTokenData.slice(0, headerBytes.length)
     if (!Arrays.equals(header, headerBytes)) {
       throw new InvalidDataException("Authentication token header not found");
     }
-    val version = decrypted.slice(headerBytes.length, headerBytes.length + versionBytes.length)
+    val version = decryptedTokenData.slice(headerBytes.length, headerBytes.length + versionBytes.length)
     if (!Arrays.equals(version, versionBytes)) {
       throw new InvalidDataException("Unsupported version " + new String(version, encodingCharset));
     }
     val payloadStart = headerBytes.length + versionBytes.length
-    val tokenData = decrypted.slice(payloadStart, decrypted.length)
+    val tokenData = decryptedTokenData.slice(payloadStart, decryptedTokenData.length)
     decodeToken(tokenData)
-  }
-  
-  def encrypt(data: Array[Byte], salt: Array[Byte]): (Array[Byte], Array[Byte]) = {
-    val spec = new PBEKeySpec(passPhrase.toCharArray, salt, 65536, 128)
-    val tmp = keyFactory.generateSecret(spec);
-    val secret = new SecretKeySpec(tmp.getEncoded, "AES")
-    val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
-    cipher.init(Cipher.ENCRYPT_MODE, secret)
-    val params = cipher.getParameters
-    val iv = params.getParameterSpec(classOf[IvParameterSpec]).getIV
-    assert(iv.length == 16)
-    (iv, cipher.doFinal(data, 0, data.length))
-  }
-  
-  def decrypt(iv: Array[Byte], data: Array[Byte], salt: Array[Byte]): Array[Byte] = {
-    require(iv.length == 16, "Invalid initialization vector length, must be 16")
-    val spec = new PBEKeySpec(passPhrase.toCharArray, salt, 65536, 128)
-    val tmp = keyFactory.generateSecret(spec);
-    val secret = new SecretKeySpec(tmp.getEncoded, "AES")
-    val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
-    cipher.init(Cipher.DECRYPT_MODE, secret, new IvParameterSpec(iv))
-    cipher.doFinal(data)
   }
 }
