e7eff51d88b0bdec3eb336f4b27d062105ab9ef9
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2013-07-30 15:58:32 +0300

    Fixed variable length integer codec

--- src/main/scala/serialization/BitOps.scala
+++ src/main/scala/serialization/BitOps.scala
@@ -13,4 +13,42 @@ object BitOps {
     val size = x & 0x3F
     (id, size)
   }
+
+  def toInt(b0: Byte, b1: Byte): Int = {
+    (b0 & 0xFF) << 8 | (b1 & 0xFF)
+  }
+
+  def toInt(b: Byte): Int =
+    b.toInt & 0xFF
+
+  def toUnsignedByte(n: Int): Byte = (n & 0xFF).toByte
+
+  def padWith(c: Char, n: Int)(str: String): String = {
+    if (str.length < n) {
+      val amountToPad = n - str.length
+      (c.toString * amountToPad) + str
+    } else str
+  }
+
+  def toBinaryString(b: Byte): String =
+    padWith('0', 8)(Integer.toBinaryString(toInt(b)))
+
+  def toGroupedBinaryString(n: Int): String =
+    Integer.toBinaryString(n).grouped(8).map(padWith('0', 8)).mkString(" ")
+
+  def toBinaryString(arr: Array[Byte]): String = {
+    val builder = arr.foldLeft(new StringBuilder) { (builder, b) ⇒
+      val x = toInt(b)
+      val toPad = Integer.numberOfLeadingZeros(x) - 24
+      for (i ← 1 to toPad) builder.append('0')
+      builder.append(Integer.toBinaryString(x)).append(' ')
+    }
+    builder.deleteCharAt(builder.length - 1)
+    builder.toString()
+  }
+
+  def toBinaryString2(arr: Array[Byte]): String = {
+    val str = arr.foldLeft("")((acc, b) ⇒ acc + " " + toBinaryString(b))
+    str.tail
+  }
 }
--- src/test/scala/serialization/VariableLengthIntegerCodecTest.scala
+++ src/test/scala/serialization/VariableLengthIntegerCodecTest.scala
@@ -2,29 +2,48 @@ package serialization
 
 import org.scalatest.FunSpec
 import org.scalatest.matchers.ShouldMatchers
+import BitOps._
+import scala.util.Random
 
 class VariableLengthIntegerCodecTest extends FunSpec with ShouldMatchers {
+  import VariableLengthIntegerCodec._
+
   describe("VariableLengthIntegerCodec") {
     it("should encode Wikipedia example number correctly") {
       val x = 137
-      val arr = VariableLengthIntegerCodec.encode(x)
+      val arr = encode(x)
       arr should have length 2
-      arr(0) should equal(-127)
-      arr(1) should equal(9)
+      toInt(arr(0)) should equal(129)
+      toInt(arr(1)) should equal(9)
     }
 
-    /*it("should encode a two-byte integer correctly") {
-      val x = 0xCF3L
-      val arr = VariableLengthIntegerCodec.encode(x)
-      arr should have length 2
-      arr(0) should equal(0x19)
-      arr(1) should equal(0x73)
+    it("should decode Wikipedia example number correctly") {
+      val arr = Array(toUnsignedByte(129), toUnsignedByte(9))
+      val x = decode(arr)
+      x should equal(137)
     }
 
-    it("should decode a two-byte integer correctly") {
-      val arr = Array(0x19.toByte, 0x73.toByte)
-      val x = VariableLengthIntegerCodec.decode(arr)
-      x should equal(0xCF3L)
-    }*/
+    it("should encode a two-byte integer correctly") {
+      val x = 10292290
+      val arr = encode(x)
+      arr should have length 4
+      toInt(arr(0)) should equal(132)
+      toInt(arr(1)) should equal(244)
+      toInt(arr(2)) should equal(152)
+      toInt(arr(3)) should equal(66)
+    }
+
+    it("should roundtrip encode random numbers correctly") {
+      val numbers = randomNumbers.take(1000)
+      numbers foreach { n ⇒
+        decode(encode(n)) should equal(n)
+      }
+    }
+
+    def randomNumbers: Iterator[Int] = new Iterator[Int] {
+      val random = new Random
+      def hasNext = true
+      def next(): Int = random.nextInt(Int.MaxValue)
+    }
   }
 }
