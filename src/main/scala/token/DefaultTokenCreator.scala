fc22577b58380bb0ef19aac7407c25bc1b6a4e5e
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2017-03-17 16:06:21 +0200

    Use AESTokenCreator by default

--- src/main/scala/token/DefaultTokenCreator.scala
+++ src/main/scala/token/DefaultTokenCreator.scala
@@ -5,10 +5,10 @@ import java.nio.charset.Charset
 import crypto.RandomSaltGenerator
 import encoding.FieldEncoderTokenEncoder
 
-trait DefaultTokenCreator extends JasyptTokenCreator with FieldEncoderTokenEncoder with RandomSaltGenerator {
+trait DefaultTokenCreator extends AESTokenCreator with FieldEncoderTokenEncoder with RandomSaltGenerator {
   val header = "AUTH"
-  val version = 3
-  val encodingCharset = Charset.forName("UTF-8")
-  override val headerBytes = header.getBytes(encodingCharset)
-  override val versionBytes = Array[Byte](version.toByte)
+  val version = 1
+  val encodingCharset: Charset = Charset.forName("UTF-8")
+  override val headerBytes: Array[Byte] = header.getBytes(encodingCharset)
+  override val versionBytes: Array[Byte] = Array[Byte](version.toByte)
 }
